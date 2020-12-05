d3fdd24d1cd644f31fa4414bbd6d3d3423ea601d
Gnivlove <marino18barada@gmail.com>
2020-12-05 14:13:45 +0100

    zadatke sam opet poceo iz pocetka i sad rade nez zasto

--- struct/6,a.c
+++ struct/6,a.c
@@ -1,36 +1,36 @@
 
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
 #include<time.h>
+#include<stdlib.h>
 
 struct stog;
-typedef struct stog *Position;
-typedef struct stog {
+typedef struct stog* Position;
+typedef struct stog
+{
 	int el;
 	Position next;
 }Stog;
 
-void Push(Position);
 void Print(Position);
+void Push(Position);
 void Pop(Position);
+int Sort(Position);
 
 int main()
 {
+	srand(time(0));
 	Stog head;
-
 	head.next = NULL;
 	int d;
-	srand(time(0));
+
 	while (1)
 	{
 		printf("\n\tMENU:\n");
 		printf("\t1-PUSH\n");
 		printf("\t2-POP\n");
-		printf("\t0-END\n\tEnter number:  ");
+		printf("\t0-END \n\tEnter number:\t");
 		scanf(" %d", &d);
-
 		switch (d)
 		{
 		case 1:
@@ -45,14 +45,12 @@ int main()
 			break;
 		case 0:
 			Print(&head);
-			puts("");
 			return 0;
-		default:
-			puts("\nInput Eror!!\n");
 			break;
+		default:
+			puts("Input error!!");
 		}
 	}
-	system("pause");
 	return 0;
 }
 
@@ -60,10 +58,11 @@ void Print(Position p)
 {
 	int i = 0;
 	p = p->next;
-	puts("List content:\n\n");
+	printf("\n\tList content:\n");
 	while (p != NULL)
 	{
-		printf("\t\t%d.  %d\n", i + 1, p->el);
+		printf("\t\t%d.   %d\n", i + 1, p->el);
+		p = p->next;
 		i++;
 	}
 }
@@ -71,34 +70,60 @@ void Print(Position p)
 void Push(Position p)
 {
 	Position q = NULL;
-	static Position last;
 	int a;
-	a = 10 + rand() % 91;
+	a= Sort(p);
 
-	//while (p->next != NULL)
-	//	p = p->next;
+	while (p->next != NULL)
+		p = p->next;
+
+	//a = 10 + rand() % 91;
 
 	q = (Position)malloc(sizeof(Stog));
 	q->el = a;
-
-	if (p->next == NULL)
-		last = p;
-
-	q->next = last->next;
-	last->next = q;
-	last = q;
+	q->next = p->next;
+	p->next = q;
 }
 
 void Pop(Position p)
 {
-	Position temp = p->next;
+	Position temp;
+	temp = p;
 
-	while (temp != NULL)
+	while (temp->next != NULL)
 	{
 		p = temp;
 		temp = temp->next;
 	}
-
 	p->next = temp->next;
 	free(temp);
+}
+
+int Sort(Position p)   // sortira niz tako da se u stog ne spremu dva ista rendum broja
+{
+	Position restart = p;   // varijabla za restartirat virjednost p
+	int a;
+	int True = 1;			// predpostavka koje ja tocna ako nema istih elemenata u redu
+	a = 10 + rand() % 91;
+
+	while (1)
+	{
+		if (p->el == a)
+		{
+			a = 10 + rand() % 91;			//ako ima istih elemenata predpostavka je kriva 
+			True = 0;						// te generiraj opet neki rendum broj
+		}
+
+		if (p->next == NULL && True == 1)		// uvet stajanja ove petlje je ako se dodjemo na kraj liste
+			break;								// te predpostavka je istinita ona to znaci da je taj element jedinstven
+
+		if (p->next == NULL)
+		{
+			p = restart;					// ako smo dosli do kraja liste i predpostavka nije tocna 
+			True = 1;						// resetiraj vrijednosti p i resetiraj vrijednost prepostavke
+		}
+
+		p = p->next;			
+	}
+
+	return a;
 }
